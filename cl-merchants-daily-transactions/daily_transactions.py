02780b039577cdc56725563c16647bb0036b4af8
Raimundo Cavada <raimundo.cavada@sumup.com>
2022-05-19 13:07:57 -0400

    fix indentation

--- cl-merchants-daily-transactions/daily_transactions.py
+++ cl-merchants-daily-transactions/daily_transactions.py
@@ -30,19 +30,19 @@ if not prod:
         days.append([(start_date_dt + datetime.timedelta(days=i)).strftime("%Y-%m-%d 00:00:00"), (start_date_dt + datetime.timedelta(days=i+1)).strftime("%Y-%m-%d 00:00:00")])
 
     for start_date, end_date in days:
-            print(f'Running from {start_date} to {end_date}')
-            query_name = 'merchants_daily_transactions'
-            extracted_data = dwh().dwh_to_pandas(
-                filename=path.join('querys','server_querys', f'select_{query_name}.sql'),
-                _start_date = start_date,
-                _end_date = end_date)
+        print(f'Running from {start_date} to {end_date}')
+        query_name = 'merchants_daily_transactions'
+        extracted_data = dwh().dwh_to_pandas(
+            filename=path.join('querys','server_querys', f'select_{query_name}.sql'),
+            _start_date = start_date,
+            _end_date = end_date)
 
-            dwh().pandas_to_dwh(
-                        dataframe=extracted_data,
-                        schema_name='analyst_acquisition_cl',
-                        table_name='merchants_daily_transactions',
-                        if_exists='append'
-                    )
+        dwh().pandas_to_dwh(
+                    dataframe=extracted_data,
+                    schema_name='analyst_acquisition_cl',
+                    table_name='merchants_daily_transactions',
+                    if_exists='append'
+                )
 if prod:
 
     santiago_tz = pytz.timezone('America/Santiago')
