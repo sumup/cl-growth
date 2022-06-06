971f541d2de641b1ada880e94803c219565a2dc6
Raimundo Cavada <raimundo.cavada@sumup.com>
2022-06-05 21:44:35 -0400

    add order by to query

--- cl-payback-acquisition/querys/snowflake/select_unit_economics.sql
+++ cl-payback-acquisition/querys/snowflake/select_unit_economics.sql
@@ -5,3 +5,5 @@ metric__google_sheets as metric,
 value__google_sheets as value
 from "SHARED_FUNNEL_SUMUP__LM3JD3KWKTSKJUJGEZZ"."FUNNEL__LM3JD3KWKTSKJUJGEZZ"."CL_UNITS_ECONOMICS"
 where month__google_sheets = '_month'
+and metric__google_sheets != 'Budget'
+order by metric__google_sheets
