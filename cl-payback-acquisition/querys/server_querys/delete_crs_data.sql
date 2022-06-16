3cce71020fbe02cda0c6a6879b485f1f60dea8ea
Raimundo Cavada <raimundo.cavada@sumup.com>
2022-06-16 10:21:09 -0400

    fix: delete

--- cl-payback-acquisition/querys/server_querys/delete_crs_data.sql
+++ cl-payback-acquisition/querys/server_querys/delete_crs_data.sql
@@ -1,2 +1,3 @@
 delete from analyst_acquisition_cl.growth_sales gs
 where gs.date >= '_date'
+returning *
--- cl-payback-acquisition/querys/server_querys/delete_forecast_data.sql
+++ cl-payback-acquisition/querys/server_querys/delete_forecast_data.sql
@@ -1,2 +1,3 @@
 delete from analyst_acquisition_cl.growth_forecast_data gfd
 where gfd.date >= '_date'
+returning *
