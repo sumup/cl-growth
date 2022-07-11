840741dc219de1cd6cfd09466775b8f4841bb016
Raimundo Cavada <raimundo.cavada@sumup.com>
2022-07-11 09:07:12 -0400

    filter resolved ones

--- cl-payback-acquisition/querys/server_querys/select_wrong_budget.sql
+++ cl-payback-acquisition/querys/server_querys/select_wrong_budget.sql
@@ -13,6 +13,7 @@ gfd.avg_monthly_net_rev_ncro,
 gfd.crs_net_landed_cost
 from analyst_acquisition_cl.growth_forecast_data gfd
 left join analyst_acquisition_cl.growth_acquisition_budget gab on gab."date" = gfd."date" and gab.acq_channel_level_1 = gfd.acq_channel_level_1 
+where gfd.proportional_budget = 0
 group by 1,2,3,4,5,6,7,8,9,10,12,13
 having sum(gfd.ncro_payback)  = 0
 and sum(gab.amount_spent) is not null
